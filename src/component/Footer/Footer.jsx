8ebe8ae00734e76947eead6a4190dff505e65245
mohit <mohitsasane76@gmail.com>
2024-04-10 11:09:14 +0530

    footer created successfully

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../Footer/Footer.css";
+import logo from "../../assets/epicLogo.png";
+import Unreal from "../../assets/Unreal Engine.svg"
+
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -79,6 +82,38 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
+        <hr/>
+
+        <div className="paraDiv">
+          <div className="para">© 2024, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners.</div>
+          <div className="para">Our websites may contain links to other sites and resources provided by third parties. These links are provided for your convenience only. Epic Games has no control over the contents of those sites or resources, and accepts no responsibility for them or for any loss or damage that may arise from your use of them.</div>
+        </div>
+
+        <div className="policy_section">
+          <div className="policy">
+            <ol>
+              <li>Terms of Service</li>
+              <li>Privacy Policy</li>
+              <li>Store Refund Policy</li>
+            </ol>
+          </div>
+
+
+
+          <div className="epic_unreal">
+
+          <div className="logo_policy">
+          <img src={logo} alt="logo" />
+          </div>
+
+          <div className="logo_policy">
+          <img src={Unreal} alt="unreal" />
+          </div>
+          </div>
+          
+        </div>
+
       </section>
     </>
   );
