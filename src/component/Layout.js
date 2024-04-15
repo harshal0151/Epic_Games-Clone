b883da4cb706407a8124308e8b61c838850ecbd2
mohit <mohitsasane76@gmail.com>
2024-04-15 10:01:42 +0530

    component added in home page

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,25 +1,20 @@
 import React from "react";
-import Header from "./header/Header"
-// import Home from "../page/Home";
-// import Brows from "../page/Brows";
-import Footer from "../component/Footer/Footer"
+import Header from "./header/Header";
+
+import Footer from "../component/Footer/Footer";
 import Routers from "../router/Routers";
-import Deals from "./deals-of-week/Deals";
 
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div className="routes">
+        <Routers />
+      </div>
 
-const Layout = () =>{
-    return(
-        <> 
-           <Header/>
-          <div className="routes">
-            <Routers/>
-            <Deals/>
-          </div>
-
-          
-           <Footer/>
-        </>
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,12 +3,13 @@
 import React from "react";
 import Heroslider from "../component/Sliders/Heroslider";
 import FeaturedDiscounts from "../component/featuredDIs/FeaturedDis";
-
+import Deals from "../component/deals-of-week/Deals";
 const Home = () =>{
     return(
         <>
        <Heroslider/>
        <FeaturedDiscounts/>
+       <Deals/>
         </>
             
           
